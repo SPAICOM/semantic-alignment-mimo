f0586d5d35c05163ee31bbf8aa4d97eee2eccc5b
JRhin <github@jrhin.com>
2024-04-02 20:28:03 +0200

    Pytorch Dataset

--- src/datamodule.py
+++ src/datamodule.py
@@ -1,7 +1,8 @@
+import torch
 import polars
 from pathlib import Path
-import pytorch_lightning as pl
 from torch.utils.data import Dataset, DataLoader
+from pytorch_lightning import LightningDataModule
 
 
 
@@ -13,23 +14,44 @@ from torch.utils.data import Dataset, DataLoader
 
 class CustomDataset(Dataset):
     def __init__(self,
-                 path: Path):
+                 path: Path,
+                 columns: list[str],
+                 target: str):
         self.path = path
-        self.dataframe = polars.read_csv(path)
+        self.columns = columns
+        self.target = target
+        self.dataframe = polars.scan_parquet(path).select(self.columns + [self.target])
 
 
     def __len__(self) -> int:
-        """Returns the length of the Dataset.
+        """Returns the length of the Dataset in a lazy fashion.
 
         Returns:
             - int : Length of the Dataset.
-        """"
-        ...
+        """
+        return self.dataframe.count().select(self.target).collect().item()
 
 
     def __getitem__(self,
-                    idx: int):
-        ...
+                    idx: int) -> tuple[torch.Tensor, torch.Tensor]:
+        """Returns in a torch.Tensor format the inputs and the target from a specific row with id equal to idx, all in a lazy fashion.
+
+        Args:
+            - idx (int): The index of the wanted row.
+
+        Returns:
+            - tuple[torch.Tensor, torch.Tensor] : The inputs and target as a tuple of tensors.
+        """
+        # Get row by index in a lazy fashion
+        row = self.dataframe.slice(idx, 1).collect().row(0, named=True) 
+
+        # Get the input
+        inputs = [elem for col in self.columns for elem in row[col]]
+
+        # Get the target
+        target = row[self.target]
+
+        return torch.tensor(inputs, dtype=torch.float), torch.tensor(target, dtype=torch.float)
 
 
 
@@ -39,7 +61,7 @@ class CustomDataset(Dataset):
 #
 # =====================================================
     
-class DataModule(pl.LightningDataModule):
+class DataModule(LightningDataModule):
     def __init__(self,
                  path : str,
                  batch_size: int = 128) -> None:
