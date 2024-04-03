7f033fa500562dc0e75b8489b8a8baec409c39ef
JRhin <github@jrhin.com>
2024-04-03 16:28:21 +0200

    downgraded from nixos-unstable to nixos-23.11 for the xz trojan

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
  description = "A Python 3.11 environment with PyTorch for scripts";
 
- inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+ inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
  outputs = { self, nixpkgs }: {
     devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
