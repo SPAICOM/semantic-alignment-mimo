699def9b1a8d584141b2d632ab97ae2a8a41d670
JRhin <github@jrhin.com>
2024-04-05 23:15:09 +0200

    nix.flakes updated from nixos-23.11 to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -1,17 +1,20 @@
 {
  description = "A Python 3.11 environment with PyTorch for scripts";
 
- inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+ inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
  outputs = { self, nixpkgs }: {
     devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
       buildInputs = [
         (python311.withPackages (ps: with ps; [
+          datasets
           numpy
           pip
           polars
           pytorch-lightning
+          timm
           torch
+          torchvision
           wandb
         ]))
       ];
