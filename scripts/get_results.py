9eb8d11f05aaab959f3c352b4080dcf5549dbfad
JRhin <jrhin@proton.me>
2024-05-21 19:00:55 +0200

    added semantic auto encoder section

--- scripts/get_results.py
+++ scripts/get_results.py
@@ -12,7 +12,7 @@ from pytorch_lightning import Trainer
 from torch.utils.data import TensorDataset, DataLoader
 
 from src.linear_optim import LinearOptimizer
-from src.models import RelativeEncoder, Classifier
+from src.models import RelativeEncoder, Classifier, SemanticAutoEncoder
 from src.datamodules import DataModuleRelativeEncoder, DataModuleClassifier
 
 def main() -> None:
@@ -28,6 +28,9 @@ def main() -> None:
     trainer = Trainer(inference_mode=True, enable_progress_bar=False, logger=False)
 
     results = pl.DataFrame()
+    # ==============================================================================
+    #                      Get results for the Relative Encoder
+    # ==============================================================================
     for encoder_path in (MODELS_DIR / 'encoders').rglob('*.ckpt'):
         # Getting the settings
         _, _, dataset, encoder, decoder, function, case, seed, ckpt = str(encoder_path.as_posix()).split("/")
@@ -55,7 +58,7 @@ def main() -> None:
         #                            Classfier Stuff
         # =========================================================================
         # Define the path towards the classifier
-        clf_path: Path = MODELS_DIR / f"decoders/{dataset}/{decoder}/{function}/seed_{seed}/anchors_{anchors}.ckpt"
+        clf_path: Path = MODELS_DIR / f"classifiers/{dataset}/{decoder}/seed_{seed}/anchors_{anchors}.ckpt"
 
         # Load the classifier model
         clf = Classifier.load_from_checkpoint(clf_path)
@@ -86,7 +89,7 @@ def main() -> None:
                            'Encoder': encoder,
                            'Decoder': decoder,
                            'Function': function,
-                           'Case': f'{case} NN',
+                           'Case': f'{case} RE NN',
                            'Seed': seed,
                            'Anchors': anchors,
                            'Alignment Loss': alignment_metrics['test/loss_epoch'],
@@ -120,7 +123,7 @@ def main() -> None:
                                    'Encoder': encoder,
                                    'Decoder': decoder,
                                    'Function': function,
-                                   'Case': f'{case} linear {solver}',
+                                   'Case': f'{case} RE linear {solver}',
                                    'Seed': seed,
                                    'Anchors': anchors,
                                    'Alignment Loss': opt.eval(enc_datamodule.test_data.z, enc_datamodule.test_data.r_decoder),
@@ -130,12 +133,12 @@ def main() -> None:
                                in_place=True)
 
   
-    # ==============================================================
-    #                Get the original values
-    # ==============================================================
-    for clf_path in (MODELS_DIR / 'decoders').rglob('*.ckpt'):
+    # ==============================================================================
+    #                            Get the original values
+    # ==============================================================================
+    for clf_path in (MODELS_DIR / 'classifiers').rglob('*.ckpt'):
         # Getting the settings
-        _, _, dataset, decoder, function, seed, ckpt = str(clf_path.as_posix()).split("/")
+        _, _, dataset, decoder, seed, ckpt = str(clf_path.as_posix()).split("/")
         seed = int(seed.split('_')[-1])
         anchors = int(ckpt.split('.')[0].split('_')[-1])
 
@@ -159,7 +162,7 @@ def main() -> None:
                            'Dataset': dataset,
                            'Encoder': decoder,
                            'Decoder': decoder,
-                           'Function': function,
+                           'Function': None,
                            'Case': 'Original Model',
                            'Seed': seed,
                            'Anchors': anchors,
@@ -169,6 +172,75 @@ def main() -> None:
                        }),
                        in_place=True)
 
+    
+    # ==============================================================================
+    #                   Get results for the Semantic AutoEncoder
+    # ==============================================================================
+    for autoencoder_path in (MODELS_DIR / 'autoencoders').rglob('*.ckpt'):
+        # Getting the settings
+        _, _, dataset, encoder, decoder, case, seed, ckpt = str(autoencoder_path.as_posix()).split('/')
+        seed = int(seed.split('_')[-1])
+        anchors = int(ckpt.split('.')[0].split('_')[-1])
+
+        # Load the Semantic AutoEncoder model
+        model = SemanticAutoEncoder.load_from_checkpoint(autoencoder_path)
+        model.eval()
+
+        # Get and setup the datamodule
+        datamodule = DataModuleRelativeEncoder(dataset=dataset,
+                                               encoder=encoder,
+                                               decoder=decoder,
+                                               num_anchors=anchors,
+                                               case=case,
+                                               batch_size=batch_size)
+        datamodule.prepare_data()
+        datamodule.setup()
+        
+        # =========================================================================
+        #                            Classfier Stuff
+        # =========================================================================
+        # Define the path towards the classifier
+        clf_path: Path = MODELS_DIR / f"classifiers/{dataset}/{decoder}/seed_{seed}/anchors_{anchors}.ckpt"
+
+        # Load the classifier model
+        clf = Classifier.load_from_checkpoint(clf_path)
+        clf.eval()
+
+        # Get and setup the classifier datamodule
+        clf_datamodule = DataModuleClassifier(dataset=dataset,
+                                              decoder=decoder,
+                                              num_anchors=anchors,
+                                              batch_size=batch_size)
+        clf_datamodule.prepare_data()
+        clf_datamodule.setup()
+
+        # =========================================================================
+        #                           Computing the results
+        # =========================================================================
+        # Get the relative representation in the decoder space
+        r_psi_hat = torch.cat(trainer.predict(model=model, datamodule=datamodule))
+        alignment_metrics = trainer.test(model=model, datamodule=datamodule)[0]
+              
+        # Get the predictions using as input the r_psi_hat
+        dataloader = DataLoader(TensorDataset(r_psi_hat, datamodule.test_data.labels), batch_size=batch_size)
+        clf_metrics = trainer.test(model=clf, dataloaders=dataloader)[0]
+        
+        results.vstack(pl.DataFrame(
+                       {
+                           'Dataset': dataset,
+                           'Encoder': encoder,
+                           'Decoder': decoder,
+                           'Function': function,
+                           'Case': f'{case} SAE NN',
+                           'Seed': seed,
+                           'Anchors': anchors,
+                           'Alignment Loss': alignment_metrics['test/loss_epoch'],
+                           'Classifier Loss': clf_metrics['test/loss_epoch'],
+                           'Accuracy': clf_metrics['test/acc_epoch']
+                       }),
+                       in_place=True)
+
+       
     print(results)
     results.write_parquet('results.parquet')
     
