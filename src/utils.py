29487d6fd4698b548096abca5ee147e6aea51524
JRhin <jrhin@proton.me>
2024-05-21 10:59:47 +0200

    added complex_tensor method in utils.py

--- src/utils.py
+++ src/utils.py
@@ -13,6 +13,21 @@ import torch
 #
 # ================================================================
 
+def complex_tensor(x: torch.Tensor) -> torch.Tensor:
+    """Get the complex form of a tensor.
+
+    Args:
+        x : torch.Tensor
+            The original tensor.
+
+    Returns:
+        torch.Tensor
+            The output tensor, which is the complex form of the original tensor.
+    """
+    device = x.device
+    x = torch.stack((x, torch.zeros(x.shape).to(device)), dim=-1)
+    return torch.view_as_complex(x)
+
 def complex_gaussian_matrix(mean: float,
                             std: float,
                             size: tuple) -> torch.Tensor:
@@ -35,7 +50,7 @@ def complex_gaussian_matrix(mean: float,
     imag_part = torch.normal(mean, std/2, size=size)
 
     # Stack real and imaginary parts along the last dimensioni
-    complex_matrix = torch.stack((real_part, imag_part), dim=2)
+    complex_matrix = torch.stack((real_part, imag_part), dim=-1)
 
     return torch.view_as_complex(complex_matrix)
     
@@ -56,9 +71,17 @@ def main() -> None:
     std: float = 1.
     size: tuple[int] = (4, 4)
 
+    x = torch.randn(4, 1)
+
     print("Performing first test...", end="\t")
     complex_matrix = complex_gaussian_matrix(mean=mean, std=std, size=size)
     print("[PASSED]")
+
+    print()
+    print("Performing second test...", end="\t")
+    x = complex_tensor(x)
+    print("[PASSED]")
+
     return None
 
 
