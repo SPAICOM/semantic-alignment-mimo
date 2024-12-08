bc91b3f955ddddd6ac0f457052f35396517f0cd2
JRhin <github@jrhin.com>
2024-12-08 14:07:08 +0100

    updated wandb_model_downloader.py

--- scripts/wadb_model_downloader.py
+++ scripts/wadb_model_downloader.py
@@ -4,7 +4,22 @@ from pathlib import Path
 from tqdm.auto import tqdm
 
 
-def find_and_rename_ckpt_files(root_dir, new_dir, name):
+def find_and_rename_ckpt_files(root_dir: str,
+                               new_dir: str,
+                               name: str) -> None:
+    """Save the results in a convenient directory structure.
+
+    Args:
+        root_dir : str
+            The directory where the files are currently saved.
+        new_dir : str
+            The new directory where to save the files.
+        name : str
+            The new name of the file.
+
+    Returns:
+        None
+    """
     root_dir = Path(root_dir)
     new_dir = Path(new_dir)
 
@@ -33,9 +48,9 @@ def main() -> None:
     """
     run = wandb.init()
 
-    seeds = [42, 123, 200]
+    seeds = [27, 42, 100, 123, 144, 200]
     antennas = [1, 2, 4, 6, 8, 10, 12, 14] 
-    sigmas = [0.01, 0.1, 10., 100., 1000., 0.05, 0.5, 5., 50., 500.]
+    sigmas = [0.0001, 0.001, 0.01, 0.1, 10., 100., 1000., 0.0005, 0.005, 0.05, 0.5, 5., 50., 500.]
     costs = [1]
 
     for ant in tqdm(antennas):
@@ -43,19 +58,19 @@ def main() -> None:
             for seed in seeds:
                 run.use_artifact(f'jrhin-org/SemanticAutoEncoder_wn_{ant}_{ant}_unaware_0_{cost}/model-seed_{seed}:best', type='model').download()
 
-                find_and_rename_ckpt_files('artifacts', f'unaware-{cost}/antennas_{ant}_{ant}/sigma_0.1/', seed)
+                find_and_rename_ckpt_files('artifacts', f'unaware-{cost}/antennas_{ant}_{ant}/sigma_0.1/', str(seed))
 
             for seed in seeds:
                 run.use_artifact(f'jrhin-org/SemanticAutoEncoder_wn_{ant}_{ant}_aware_0.1_{cost}/model-seed_{seed}:best', type='model').download()
 
-                find_and_rename_ckpt_files('artifacts', f'aware-{cost}/antennas_{ant}_{ant}/sigma_0.1/', seed)
+                find_and_rename_ckpt_files('artifacts', f'aware-{cost}/antennas_{ant}_{ant}/sigma_0.1/', str(seed))
 
     for sigma in tqdm(sigmas):
         for cost in costs:
             for seed in seeds:
                 run.use_artifact(f'jrhin-org/SemanticAutoEncoder_wn_8_8_aware_{sigma}_{cost}/model-seed_{seed}:best', type='model').download()
             
-                find_and_rename_ckpt_files('artifacts', f'aware-{cost}/antennas_8_8/sigma_{sigma}/', seed)
+                find_and_rename_ckpt_files('artifacts', f'aware-{cost}/antennas_8_8/sigma_{sigma}/', str(seed))
             
     wandb.finish()
     
