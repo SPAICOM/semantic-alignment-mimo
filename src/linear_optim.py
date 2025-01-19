b729063f59d45d27e4005ec437b4e13459832384
JRhin <github@jrhin.com>
2025-01-19 12:00:07 +0100

    fixed bug of the remaining torch.sqrt

--- src/linear_optim.py
+++ src/linear_optim.py
@@ -454,7 +454,7 @@ class LinearOptimizerSAE():
         if self.snr:
             if self.snr_type == "transmitted":
                 # sigma = sigma_given_snr(self.snr, self.F @ input)
-                sigma = sigma_given_snr(self.snr, torch.ones(1)/torch.sqrt(self.antennas_transmitter))
+                sigma = sigma_given_snr(self.snr, torch.ones(1)/math.sqrt(self.antennas_transmitter))
             elif self.snr_type == "received":
                 sigma = sigma_given_snr(self.snr, A)
             else:
@@ -621,7 +621,7 @@ class LinearOptimizerSAE():
             if self.snr:
                 if self.snr_type == "transmitted":
                     # sigma = sigma_given_snr(snr=self.snr, signal= self.F @ input)
-                    sigma = sigma_given_snr(snr=self.snr, signal=torch.ones(1)/torch.sqrt(self.antennas_transmitter))
+                    sigma = sigma_given_snr(snr=self.snr, signal=torch.ones(1)/math.sqrt(self.antennas_transmitter))
                 elif self.snr_type == "received":
                     sigma = sigma_given_snr(snr=self.snr, signal= z)
                 else:
